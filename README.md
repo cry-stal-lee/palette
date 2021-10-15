ce7374b37a986f3171a17a14ad505076e53b812d
Crystal Lee <crystallee@Crystals-MBP.hsd1.ca.comcast.net>
2021-10-14 17:59:52 -0700

    Smoother palette svg, adjusted colors, added image uploader & selectedImage display

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 # resources used
 https://stackoverflow.com/questions/63225977/css-how-to-stack-two-div-elements-on-top-of-one-another-that-resize-together
+https://academind.com/tutorials/reactjs-image-upload
+https://medium.com/@650egor/react-30-day-challenge-day-2-image-upload-preview-2d534f8eaaa
--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,48 +1,37 @@
-import React from "react";
-import Pokemons from "./features/Pokemons";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 import paletteSvg from '../assets/palette.svg';
 
 export default function App() {
+  const [selectedFile, setSelectedFile] = useState("");
+
+  const handleUpload = e => {
+    const [file] = e.target.files;
+    setSelectedFile(() => URL.createObjectURL(file));
+  }
+
+
+  const euclideanDistance = (a, b) => {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        sum += Math.pow(b[i] - a[i], 2);
+    }
+    return Math.sqrt(sum);
+  }
+
   return (
-    <div className="App">
-      {/* <img src={logo} className="App-logo" alt="logo" /> */}
-      <img src={paletteSvg} alt='palette' className="Palette"/>
-      {/* <p>Hello!</p>
-      <Pokemons />
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://redux-toolkit.js.org/tutorials/rtk-query"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          RTK Query
-        </a>
-      </p> */}
+    <div className="app">
+    <a className="credit" href="http://crystallee.dev">crystal lee</a>
+    <div className="palette" style={{ backgroundImage: `url(${paletteSvg})` }}>
+      <div className="uploader">
+        <input type="file" accept="image/*" multiple={false} onChange={handleUpload} />
+      </div>
+      <div className="color"></div>
+      </div>
+    <div className="uploaded-image">
+      <img src={ selectedFile }/>
+    </div>
     </div>
   );
 }
