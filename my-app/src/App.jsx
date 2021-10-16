903f7966ff8e243449e58a166994f341298167fd
Crystal Lee <crystallee@Crystals-MBP.hsd1.ca.comcast.net>
2021-10-16 09:21:50 -0700

    Added title icon

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import useImageColor from 'use-image-color';
+
+import { FiCopy } from 'react-icons/Fi';
 
 import axios from 'axios';
 
@@ -9,6 +12,15 @@ import paletteSvg from '../assets/palette.svg';
 export default function App() {
   let [selectedFile, setSelectedFile] = useState(() => "");
   let [currentPalette, setCurrentPalette] = useState(() => []);
+  const { colors } = useImageColor(selectedFile, { cors: true, colors: 5 });
+  let [backgroundColors, setBackgroundColors] = useState(['#d3c9b8', '#debda5', '#e4b69a']);
+
+  useEffect(() => {
+    setCurrentPalette(colors);
+    if (colors) {
+      setBackgroundColors([currentPalette[0], currentPalette[1], currentPalette[2]])
+    };
+  }, [colors]);
 
   const handleUpload = e => {
     const file = e.target.files[0];
@@ -23,24 +35,31 @@ export default function App() {
     })
   };
 
-  // useEffect(() => {
-  //   console.log(`Color: ${color}`);
-  //   console.log(`Palette: ${palette}`);
-  // }, [palette, color]);
+  const handleClean = () => {
+    setSelectedFile('');
+    setCurrentPalette([]);
+    setBackgroundColors(['#d3c9b8', '#debda5', '#e4b69a']);
+  }
 
   return (
-    <div className="app">
-    <a className="credit" href="http://crystallee.dev">crystal lee</a>
-    <div className="palette" style={{ backgroundImage: `url(${paletteSvg})` }}>
-      <div className="uploader">
-        <input type="file" accept="image/*" multiple={false} onChange={handleUpload} onClick={e => e.target.value = null} />
+    <>
+    <div className="app" style={{ backgroundImage: `linear-gradient(${backgroundColors[0]}, ${backgroundColors[1]}, ${backgroundColors[2]})` }}> 
+      <a className="credit" href="http://crystallee.dev">crystal lee</a>
+      <div className="palette" style={{ backgroundImage: `url(${paletteSvg})` }}>
+        <div className="uploader">
+          <input type="file" accept="image/*" multiple={false} onChange={handleUpload} onClick={e => e.target.value = null} />
+          <button onClick={handleClean}>clean palette</button>
+        </div>
+        {/* {currentPalette ? <><div className="color color-1" style={{ backgroundColor: currentPalette[0] }} />
+        <div className="color color-2 " style={{ backgroundColor: currentPalette[1] }}><div>{currentPalette[1]}<FiCopy onClick={() => {navigator.clipboard.writeText(currentPalette[1])}} /></div></div>
+        <div className="color color-3" style={{ backgroundColor: currentPalette[2] }} />
+        <div className="color color-4" style={{ backgroundColor: currentPalette[3] }} />
+        <div className="color color-5" style={{ backgroundColor: currentPalette[4] }} /></>
+        : null} */}
       </div>
-      <div className="color"></div>
-      
+      <div className="uploaded-image">
+        <img src={ selectedFile }/>
       </div>
-    <div className="uploaded-image">
-      <img src={ selectedFile }/>
     </div>
-    </div>
-  );
+  </>);
 }
